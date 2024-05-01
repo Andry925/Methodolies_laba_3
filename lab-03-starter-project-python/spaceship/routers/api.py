6a80a32065ffb8bd016ed7e9d4c63e2932c66ec4
andrew <andry925@ukr.net>
2024-05-01 15:10:13 +0300

    Implement matrix endpoint

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/multiplied_matrix')
+def get_multiplied_matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    multiplied_matrix = numpy.dot(matrix_a, matrix_b)
+    final_result = {
+        "maxtrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "final_result": multiplied_matrix.tolist()
+    }
+    return final_result
+
+
+
